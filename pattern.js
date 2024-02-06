5d6b08baa329ed00d158204091e74cb36fd391d8
Owais <owais753@gmail.com>
2024-02-06 22:28:44 +0530

    initial commit

---
+++ pattern.js
@@ -0,0 +1,71 @@
+// Here i am gonna practice the "PATTERN" Questions
+
+// Q1 -> Right Angled Triangle Pattern
+
+// let n = 7;
+// for (i = 1; i <= n; i++) {
+//     for (j = 1; j <= i; j++) {
+//         process.stdout.write("*")
+//     }
+//     console.log();
+// }
+
+/*
+
+OUTPUT -
+
+*
+**
+***
+****
+*****
+******
+
+*/
+
+// Q2 -> Right Angled Triangle Pattern (Reverse)
+
+// let n = 7;
+// for (i = 1; i <= n; i++) {
+//     for (j = i; j <= n; j++) {
+//         process.stdout.write("*")
+//     }
+//     console.log(" ")
+// }
+
+/*
+
+OUTPUT -
+
+*****
+****
+***
+**
+*
+
+*/
+
+
+// Q3 -> Right Angled Triangle Pattern (Reverse(Reverse))
+
+// let n = 5;
+// for (i = 1; i <= n; i++) {
+//     for (j = n; j >= 1; j--) {
+//         process.stdout.write("*")
+//     }
+//     console.log()
+// }
+
+/*
+
+OUTPUT - 
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
+
+
